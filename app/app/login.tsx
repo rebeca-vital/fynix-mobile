e4d70d4f6628482cdd54a801b022a85ef4f4bb4a
Pedro Henrique Reis Felix <ph7829135@gmail.com>
2026-05-23 21:11:19 -0300

    criação da tela login

---
+++ app/app/login.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import {
+    Image,
+    StyleSheet,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    View,
+} from 'react-native';
+export default function Login() {
+  const [login, setLogin] = React.useState("");
+  const [senha, setSenha] = React.useState("");
+  function handleLogin() {
+    alert("Botão entrar funcionando!");
+  }
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require('../assets/images/fynix.jpeg')}
+        style={styles.logo}
+      />
+      <TextInput
+        placeholder="Digite seu email"
+        style={styles.input}
+        value={login}
+        onChangeText={setLogin}
+      />
+      <TextInput
+        placeholder="Digite sua senha"
+        style={styles.input}
+        value={senha}
+        onChangeText={setSenha}
+        secureTextEntry={true}
+      />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={handleLogin}
+      >
+        <Text style={styles.buttonText}>
+          Entrar
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 20,
+    backgroundColor: '#FFFFFF',
+  },
+  logo: {
+    width: 220,
+    height: 120,
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    marginBottom: 30,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 10,
+    padding: 15,
+    marginBottom: 15,
+  },
+  button: {
+    backgroundColor: '#FF6B00',
+    padding: 15,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
